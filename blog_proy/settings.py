a775a55283768c70b0dc5b5490938804a26bedf7
angela <angela@ubuntu.(none)>
2016-04-11 10:36:00 +0200

    base de datos cambiada en los settings

--- blog_proy/settings.py
+++ blog_proy/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blog_proy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'blog_django_bbdd',
+        'USER': 'angela',
+        'PASSWORD': 'a1234567',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
